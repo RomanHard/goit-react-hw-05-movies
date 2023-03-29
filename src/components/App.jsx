92e5baab0de3013ec02dd1fcc5fbb58d2d5c0b54
Roman <romeenice@gmail.com>
2023-03-29 21:22:03 +0300

    pull

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,22 @@
 import { NavLink, Route, Routes } from "react-router-dom";
+import Movies from "./components/Movies";
 
 export const App = () => {
   return (
     <div>
-     <nav>
-        <NavLink><a href="/">Home</a></NavLink>
-        <NavLink> <a href="/Movies">Movies</a></NavLink>
-     </nav>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/Movies">Movies</NavLink>
+      </nav>
       <Routes>
         <Route path="/" element={<div>home</div>} />
-        <Route path="/Movies" element={<div>Movies</div>} />    
-        <Route path="*" element={<div>Not Found</div>} />   
-        <Route path="Cast" element={<div>Cast</div>} />
-        <Route path="Reviews" element={<div>Reviews</div>} />
+        <Route path="/Movies" element={<Movies />}>
+          <Route path="Cast" element={<div>Cast</div>} />
+          <Route path="Reviews" element={<div>Reviews</div>} />
+        </Route>
+        <Route path="*" element={<div>Not Found</div>} />
       </Routes>
     </div>
   );
 };
+
---
+++ src/components/movies/Movies.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import { searchMovies } from './API/Api';
+import MovieList from './movieList/MovieList';
+
+const MoviesPage = () => {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const location = useLocation();
+  const history = useHistory();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const queryValue = queryParams.get('query') || '';
+    setQuery(queryValue);
+  }, [location]);
+
+  useEffect(() => {
+    if (query !== '') {
+      setIsLoading(true);
+      searchMovies(query).then(movies => {
+        setMovies(movies);
+        setIsLoading(false);
+      });
+    }
+  }, [query]);
+
+  const handleSearch = newQuery => {
+    setQuery(newQuery);
+    history.push({ ...location, search: `query=${newQuery}` });
+  };
+
+  return (
+    <div>
+      <form onSubmit={e => e.preventDefault()}>
+        <input type="text" value={query} onChange={e => handleSearch(e.target.value)} />
+        <button type="submit">Search</button>
+      </form>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <MovieList movies={movies} basePath={location.pathname} />
+      )}
+    </div>
+  );
+};
+
+export default MoviesPage;
