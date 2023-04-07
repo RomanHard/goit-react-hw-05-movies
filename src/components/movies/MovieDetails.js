85d63934b6a024586efaf267fc2217b4015e8740
Roman <romeenice@gmail.com>
2023-04-07 20:56:24 +0300

    details

--- src/components/movies/MovieDetails.js
+++ src/components/movies/MovieDetails.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMovieDetails } from '../API/Api';
+import './details-module.css';
 
 const MovieDetails = () => {
   const [movie, setMovie] = useState(null);
@@ -18,11 +19,23 @@ const MovieDetails = () => {
   }
 
   return (
-    <div>
-      <h1>{movie.title}</h1>
-      <p>{movie.overview}</p>
-      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-    </div>
+    <div className='details'>
+       <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className='logo' />
+       <div className='details-content'>
+    <h1 className='title'>{movie.title}</h1>
+    <p className='userScore'>User Score: {movie.vote_average * 10}%</p>
+    <h2 className='overviewTitle'>Overview</h2>
+    <p className='overview'>{movie.overview}</p>
+    <h3 className='genresTitle'>Genres</h3>
+    <ul className='genres'>
+      {movie.genres.map(genre => (
+        <li key={genre.id} className='genre'>
+          {genre.name}
+        </li>
+      ))}
+    </ul>
+  </div>
+</div>
   );
 };
 
