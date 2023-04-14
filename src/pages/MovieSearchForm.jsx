43f4b81da501f8330e4a5c613cd5bb8f873a7c0f
Roman <romeenice@gmail.com>
2023-04-14 20:11:41 +0300

    movieSearchForm

--- src/pages/MovieSearchForm.jsx
+++ src/pages/MovieSearchForm.jsx
@@ -1,37 +1,25 @@
-import { useSearchParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { withRouter } from 'react-router-dom';
 
-import { fetchMovieByQuery } from 'components/shared/services/moviesAPI';
+const MovieSearchForm = ({ history }) => {
+  const [keyword, setKeyword] = useState('');
 
-const MovieSearchForm = () => {
-    const [movies, setMovies] = useState([]);
-    const [searchParams, setSearchParams] = useSearchParams();
-    const keyword = searchParams.get('keyword');
-    
-    useEffect(() => {
-        if (keyword === '' || keyword === null) {
-        return;
-        }
-    
-        fetchMovieByQuery(keyword).then(setMovies);
-    }, [keyword]);
-    
-    const handleSubmit = e => {
-        e.preventDefault();
-        const form = e.currentTarget;
-        setSearchParams({ keyword: form.elements.keyword.value });
-        form.reset();
-    };
-    
-    return (
-        <form onSubmit={handleSubmit}>
-        <input type="text" name="keyword" />
-        <button type="submit">Search</button>
-        <ul>
-            {movies.map(movie => (
-            <li key={movie.id}>{movie.title}</li>
-            ))}
-        </ul>
-        </form>
-    );
-    }
+  const handleSubmit = e => {
+    e.preventDefault();
+    history.push(`/movies?keyword=${keyword}`);
+    setKeyword('');
+  };
+
+  const handleChange = e => {
+    setKeyword(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="keyword" value={keyword} onChange={handleChange} />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default withRouter(MovieSearchForm);
